52571a6388bf45dbf6e1fbea596d648a5d2bdbae
MhmmdAydn <flydifferent@gmail.com>
2020-11-24 22:33:29 +0300

    Sepete ekle kısmı eklendi.

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,12 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("1.işlem");
+
+    }
+
+    public static void yeniMethod()
+    {
+        System.out.println("Yeni methodum hazır");
+        System.out.println("Sepete ekle kısımları buralar");
     }
 }
